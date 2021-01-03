b2c34bc68140f778944598f19825ee10e90bcb5d
Durgesh <michaeldurgesh@gmail.com>
2021-01-03 23:12:29 +0530

    Added database

--- ardour/settings.py
+++ ardour/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qa_fim(cd0*(d+hpi7hxb+$u8a15xse1ls&2k^k=4_941un_kb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ardourdiaries.herokuapp.com','127.0.0.1:8000']
 
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
-     'whitenoise.middleware.WhiteNoiseMiddleware',
+     #'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -83,6 +83,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
