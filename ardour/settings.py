dc7f67f36a4a83e3ba232574ca86c1cf1e4ae7bd
Durgesh <michaeldurgesh@gmail.com>
2021-01-06 01:40:12 +0530

    added

--- ardour/settings.py
+++ ardour/settings.py
@@ -84,13 +84,7 @@ DATABASES = {
     }
 }
 
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
