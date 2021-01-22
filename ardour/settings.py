afb9869dd6e5d2eb0e4649751cf25e1ae9472cf0
Durgesh <michaeldurgesh@gmail.com>
2021-01-22 23:17:56 +0530

    m

--- ardour/settings.py
+++ ardour/settings.py
@@ -121,7 +121,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Static files (CSS, JavaScript, Images)
