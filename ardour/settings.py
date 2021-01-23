88a640c3c5f061209f4da56f525cf9fe0195e971
Durgesh <michaeldurgesh@gmail.com>
2021-01-23 20:25:40 +0530

    final commit

--- ardour/settings.py
+++ ardour/settings.py
@@ -146,7 +146,7 @@ if not DEBUG:
     AWS_STORAGE_BUCKET_NAME = "ardourdiaries"
 
     AWS_S3_SIGNATURE_VERSION = 's3v4'
-    AWS_S3_REGION_NAME='us-east-2'
+    AWS_S3_REGION_NAME='ap-south-1'
     AWS_S3_FILE_OVERWRITE = False
     AWS_DEFAULT_ACL = None
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
